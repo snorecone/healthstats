c6e63866cf52f57d7fa1d76b7bd98511583022cc
assplecake <assplecake@gmail.com>
2009-12-02 21:01:24 -0800

    Stubbed out bmi percentile method

--- lib/health_calc.rb
+++ lib/health_calc.rb
@@ -11,6 +11,13 @@ module HealthCalc
       (((weight * 703) * 100) / (height ** 2)) / 100.0
     end
   end
+  
+  def bmi_percentile
+    unless self.respond_to?(:age) && self.respond_to?(:gender)
+      errormsg = "You must define an age and gender attribute in this class"
+      raise HealthCalc::AttributeError, errormsg
+    end
+  end
 end
 
 # require 'health_calc/statistics'
--- spec/spec_bmi.rb
+++ spec/spec_bmi.rb
@@ -10,26 +10,28 @@ describe 'HealthCalc' do
       end
     end
     
-    it 'should require weight attribute in class' do
-      @klass.send(:undef_method, :weight)
-      lambda { @klass.new.bmi }.should.raise(HealthCalc::AttributeError)
+    describe 'required attributes' do
+      [:height, :weight].each do |attribute|
+        it "should include #{attribute}" do
+          @klass.send(:undef_method, attribute)
+          lambda { @klass.new.bmi }.should.raise(HealthCalc::AttributeError)
+        end
+      end
     end
     
-    it 'should require height attribute in class' do
-      @klass.send(:undef_method, :height)
-      lambda { @klass.new.bmi }.should.raise(HealthCalc::AttributeError)
-    end
-    
-    it 'should return nil if weight is nil' do
-      person = @klass.new
-      person.height = 61
-      person.bmi.should.be.nil
-    end
-    
-    it 'should return nil if height is nil' do
-      person = @klass.new
-      person.weight = 61
-      person.bmi.should.be.nil
+    describe 'nil attributes' do
+      before do
+        @person = @klass.new
+        @person.height = 61
+        @person.weight = 130
+      end
+      
+      [:height, :weight].each do |attribute|
+        it "should return nil if #{attribute} is nil" do
+          @person.send("#{attribute}=", nil)
+          @person.bmi.should.be.nil
+        end
+      end
     end
     
     it 'should return the correct bmi (precision 2)' do
@@ -38,5 +40,40 @@ describe 'HealthCalc' do
       person.height = 61
       person.bmi.should == 24.56
     end
-  end  
+  end
+  
+  describe '#bmi_percentile' do
+    before do
+      @klass = Class.new do
+        include HealthCalc
+        attr_accessor :height, :weight, :gender, :age
+      end
+    end
+    
+    describe 'required attributes' do
+      [:age, :gender].each do |attribute|
+        it "should include #{attribute}" do
+          @klass.send(:undef_method, attribute)
+          lambda { @klass.new.bmi_percentile }.should.raise(HealthCalc::AttributeError)
+        end
+      end
+    end
+    
+    describe 'nil attributes' do
+      before do
+        @person = @klass.new
+        @person.height = 61
+        @person.weight = 130
+        @person.gender = 'f'
+        @person.age = 13
+      end
+      
+      [:age, :gender].each do |attribute|
+        it "should return nil if #{attribute} is nil" do
+          @person.send("#{attribute}=", nil)
+          @person.bmi_percentile.should.be.nil
+        end
+      end
+    end
+  end
 end
